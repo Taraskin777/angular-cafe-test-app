f6756d7036c025bdf3ad98884a353b0c7fb90c6f
Taras Noga <taraskin2012@gmail.com>
2024-01-29 18:36:55 +0200

    added subscribe using asyncPipe

--- src/app/menu/components/categories/categories.component.ts
+++ src/app/menu/components/categories/categories.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, DestroyRef } from '@angular/core';
 import { DataService } from 'src/app/core/services/data.service';
 import { Categories } from 'src/app/shared/interfaces/categories';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-categories',
@@ -9,25 +10,30 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
   styleUrls: ['./categories.component.css'],
 })
 export class CategoriesComponent implements OnInit {
-  categories: Categories[] = [];
+  // categories: Categories[] = [];
+  categories$: Observable<Categories[]> | undefined;
 
   constructor(
-    private dataService: DataService,
-    private destroyRef: DestroyRef
+    private dataService: DataService
+    // private destroyRef: DestroyRef,
   ) {}
 
+  // ngOnInit(): void {
+  //   this.dataService
+  //     .getData()
+  //     .pipe(takeUntilDestroyed(this.destroyRef))
+  //     .subscribe({
+  //       next: (data: Categories[]): void => {
+  //         this.categories = data;
+  //       },
+  //       error: (error: any) => {
+  //         console.error(error);
+  //       },
+  //     });
+  // }
+
   ngOnInit(): void {
-    this.dataService
-      .getData()
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe({
-        next: (data: Categories[]): void => {
-          this.categories = data;
-        },
-        error: (error: any) => {
-          console.error(error);
-        },
-      });
+    this.categories$ = this.dataService.getData();
   }
 
   trackByCategory(index: number, item: Categories): number {
