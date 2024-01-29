ab415ce2b575c2752f7586567762124ebd2061a6
Taras Noga <taraskin2012@gmail.com>
2024-01-29 18:48:27 +0200

    removed commented code

--- src/app/menu/components/categories/categories.component.ts
+++ src/app/menu/components/categories/categories.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, DestroyRef } from '@angular/core';
 import { DataService } from 'src/app/core/services/data.service';
 import { Categories } from 'src/app/shared/interfaces/categories';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -10,27 +9,9 @@ import { Observable } from 'rxjs';
   styleUrls: ['./categories.component.css'],
 })
 export class CategoriesComponent implements OnInit {
-  // categories: Categories[] = [];
   categories$: Observable<Categories[]> | undefined;
 
-  constructor(
-    private dataService: DataService
-    // private destroyRef: DestroyRef,
-  ) {}
-
-  // ngOnInit(): void {
-  //   this.dataService
-  //     .getData()
-  //     .pipe(takeUntilDestroyed(this.destroyRef))
-  //     .subscribe({
-  //       next: (data: Categories[]): void => {
-  //         this.categories = data;
-  //       },
-  //       error: (error: any) => {
-  //         console.error(error);
-  //       },
-  //     });
-  // }
+  constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
     this.categories$ = this.dataService.getData();
