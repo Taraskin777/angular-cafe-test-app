1777ef76f39e158427db8593278206f005781cac
Taras Noga <taraskin2012@gmail.com>
2024-01-30 11:30:25 +0200

    added main and category pages

--- src/app/menu/menu-routing.module.ts
+++ src/app/menu/menu-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryComponent } from './pages/category/category.component';
+import { MainComponent } from './pages/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'category', component: CategoryComponent },{ path: '', component: MainComponent}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class MenuRoutingModule { }
+export class MenuRoutingModule {}
--- src/app/menu/menu.module.ts
+++ src/app/menu/menu.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { MenuRoutingModule } from './menu-routing.module';
 import { CategoriesComponent } from './components/categories/categories.component';
+import { MainComponent } from './pages/main/main.component';
+import { CategoryComponent } from './pages/category/category.component';
 
 @NgModule({
-  declarations: [CategoriesComponent],
+  declarations: [CategoriesComponent, MainComponent, CategoryComponent],
   imports: [CommonModule, MenuRoutingModule],
-  exports: [CategoriesComponent],
+  exports: [CategoriesComponent, MainComponent],
 })
 export class MenuModule {}
