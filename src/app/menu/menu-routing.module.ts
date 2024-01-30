e9b6f4f1c406a316575fc4b20446d3942e08ce80
Taras Noga <taraskin2012@gmail.com>
2024-01-30 12:58:39 +0200

    added method getDishesFromCategory, show page with dishes after click on particular category

--- src/app/menu/menu-routing.module.ts
+++ src/app/menu/menu-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CategoryComponent } from './pages/category/category.component';
 import { MainComponent } from './pages/main/main.component';
 
-const routes: Routes = [{ path: 'category', component: CategoryComponent },{ path: '', component: MainComponent}];
+const routes: Routes = [{ path: 'categories/:id', component: CategoryComponent },{ path: '', component: MainComponent}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/menu/pages/category/category.component.ts
+++ src/app/menu/pages/category/category.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/core/services/data.service';
+import { Dishes } from 'src/app/shared/interfaces/dishes';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit {
+  dishes$: Observable<Dishes[]> | undefined;
 
+  constructor(
+    private dataService: DataService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    console.log(id);
+    this.dishes$ = this.dataService.getDishesFromCategory(id);
+    console.log(this.dishes$);
+  }
+
+  trackByDishes(index: number, item: Dishes): number {
+    return item.id;
+  }
 }
