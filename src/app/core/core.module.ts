895027117a97b1d78ba77df6b896ae85818e4ad3
Taras Noga <taraskin2012@gmail.com>
2024-01-31 14:29:01 +0200

    added login page

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,18 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { AuthModule } from './auth/auth.module';
+import { CoreRoutingModule } from './core-routing.module';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, MatIconModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    AuthModule,
+    CoreRoutingModule,
+  ],
   exports: [HeaderComponent],
 })
 export class CoreModule {}
