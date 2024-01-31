614bf49ec85e07ddb370fc69e814503bd9c61ead
Taras Noga <taraskin2012@gmail.com>
2024-01-31 18:43:36 +0200

    added validation

--- src/app/core/auth/login/login.component.ts
+++ src/app/core/auth/login/login.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  profileForm = new FormGroup({
+    login: new FormControl('', [Validators.required]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
 
+  onSubmit(): void {
+    console.log(this.profileForm.value);
+  }
 }
