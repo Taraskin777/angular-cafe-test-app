cb19102026204a3af46622b9c20a6c4b96f7c5e8
Taras Noga <taraskin2012@gmail.com>
2024-02-08 13:24:58 +0200

    added empty methods to DishesService

--- src/app/core/services/dishes.service.ts
+++ src/app/core/services/dishes.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, throwError, BehaviorSubject } from 'rxjs';
+import { environment } from 'src/environment/environment';
+import { Dishes } from 'src/app/shared/interfaces/dishes';
+import { of } from 'rxjs';
+
+interface NewDish {
+  categoryId: number;
+  name: string;
+  image: string;
+  description: string;
+  price: number;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DishesService {
+  url: string = environment.url;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  public addDish(dish: NewDish) {}
+
+  public removeDish(dishId: number) {}
+
+  public updateDish(dishId: number, updateDish: Dishes) {}
 }
